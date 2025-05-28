5a5a4f7d1b449909a02016a6d736fe725908a279
Madiha Wazir <ahmardiha@gmail.com>
2025-05-27 22:53:07 -0400

    Updated

--- routes/clothingItem.js
+++ routes/clothingItem.js
@@ -1,9 +1,21 @@
 const router = require("express").Router();
 const clothingItem = require("../models/clothingItem");
-const { createItem } = require("../controllers/clothingItem");
+const {
+  createItem,
+  getItems,
+  deleteItem,
+  likeItem,
+  unlikeItem,
+} = require("../controllers/clothingItem");
 
 router.use("/items", clothingItem);
 
 router.post("/", createItem);
+
+router.get("/", getItems);
+
+router.put("/:itemId", likeItem);
+
+router.delete("/:itemId", unlikeItem);
 
 module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,7 @@ const clothingItem = require("./clothingItem");
 const userRouter = require("./users");
 
 router.use("/users", userRouter);
+router.use("/items", clothingItem);
 
 router.use((req, res) => {
   res.status(500).send({ message: "Router not found" });
