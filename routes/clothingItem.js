17bec5fffadaf53fa55854b996bcca67673baae0
Madiha Wazir <ahmardiha@gmail.com>
2025-08-28 17:51:06 -0400

    "Sprint 14"

--- routes/clothingItem.js
+++ routes/clothingItem.js
@@ -13,7 +13,7 @@ const {
 
 const router = express.Router();
 
-router.post("/items", validateCreateClothingItem, createItem);
+router.post("/", validateCreateClothingItem, createItem);
 
 router.delete("/:itemId", validateItemId, deleteItem);
 router.put("/:itemId/likes", validateItemId, likeItem);
