0db5fd1ba4e2f818447928f1ea1c3ae6e1db91fe
Madiha Wazir <ahmardiha@gmail.com>
2025-09-17 14:11:41 -0400

    'Sprint 15'

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const { errors } = require("celebrate");
+require("dotenv").config();
 
 const routes = require("./routes/index");
 
